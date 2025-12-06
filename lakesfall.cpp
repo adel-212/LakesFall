dd0e121db34e0e27b1f367f4c6c7913e12dfb6d8
El Moussaoui Adel <adelmoussaoui04@gmail.com>
2025-12-06 17:39:29 +0100

    fix

--- lakesfall.cpp
+++ lakesfall.cpp
@@ -1,24 +1,36 @@
-#include stdio.h>
+#include <stdio.h>
 #include <vector>
-
+#include <iostream>
 using namespace std;
 
-int main(){
-    vector<int> Liste;
-    int SommetTailleMax; 
+int main() {
+    int t;
+    scanf("%d", &t); // On lit le nombre de cas
 
+    while (t--) { // Pour chaque cas de test...
+        int n;
+        scanf("%d", &n); // On lit la taille de CET histogramme
 
-    for(int i = 0; i < 100000; i++){
-        int valeur;
-        scanf("%d", &valeur);
-        Liste.push_back(valeur);
-    }
-
-    SommetTailleMax = Liste[0];
-    for(int i = 1; i < Liste.size(); i++){
-        if(Liste[i] > SommetTailleMax){
-            SommetTailleMax = Liste[i];
+        vector<int> Liste;
+        // ... ICI : Boucle pour lire les 'n' hauteurs et les mettre dans Liste ...
+        for (int i = 0; i < n; i++) {
+            int h;
+            scanf("%d", &h);
+            Liste.push_back(h);
         }
 
-    
+        // ... ICI : Recherche de l'INDEX du maximum ...
+        int IndexSommetMax = 0;
+        // ... ta boucle for qui cherche le max ...
+        for (int i = 1; i < n; i++) {
+            if (Liste[i] > Liste[IndexSommetMax]) {
+                IndexSommetMax = i;
+            }
+        }
+        
+        // C'est ici qu'on mettra la logique des Lacs (Gauche et Droite) plus tard
+        
+        // À la fin de chaque cas, on affiche le résultat (0 0 0 pour l'instant)
     }
+    return 0;
+}
