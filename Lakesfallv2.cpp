c8a328935cf8ea0d7fd98876be644b005c83b058
El Moussaoui Adel <adelmoussaoui04@gmail.com>
2025-12-07 16:15:31 +0100

    evol

--- Lakesfallv2.cpp
+++ Lakesfallv2.cpp
@@ -18,6 +18,7 @@ int main() {
             Liste.push_back(Mur);
         }
 
+        // Recherche de lindex du sommet maximum
         int IndexSommetMax = 0;
         for (int i = 1; i < TailleHistogramme; i++) {
             if (Liste[i] > Liste[IndexSommetMax]) {
@@ -25,25 +26,27 @@ int main() {
             }
         }
         
-        // 1. Les Champions
+        // initialisation des variable de résultat
         int VolumeMax = 0, DebutVolume = 0, FinVolume = 0;
         int LargeurMax = 0, DebutLargeur = 0, FinLargeur = 0;
         int ProfondeurMax = 0, DebutProfondeur = 0, FinProfondeur = 0;
 
-        // 2. Préparation
+        // initialisation des variables temporaire a 0
         int IndexMurReference = 0;
         int VolumeTemp = 0;
         int ProfondeurMaxTemp = 0;
 
-        // 3. BOUCLE GAUCHE (0 -> Sommet)
+        // boucle gauche du debut jusquau sommet Max
         for (int i = 0; i <= IndexSommetMax; i++) {
             
+            // cas ou on est dans leau pck cest plus petit que le mur de reference
             if (Liste[i] < Liste[IndexMurReference]) {
-                VolumeTemp += (Liste[IndexMurReference] - Liste[i]);
+                VolumeTemp = VolumeTemp + (Liste[IndexMurReference] - Liste[i]);
                 int ProfondeurActuelle = Liste[IndexMurReference] - Liste[i];
                 if (ProfondeurActuelle > ProfondeurMaxTemp) {
                     ProfondeurMaxTemp = ProfondeurActuelle;
                 }
+            // cas ou le lac est fini pck on a un mur plus grand ou egal au mur de reference
             } else {
                 if (VolumeTemp > 0) {
                     if (VolumeTemp > VolumeMax) {
@@ -68,18 +71,18 @@ int main() {
                 VolumeTemp = 0;
                 ProfondeurMaxTemp = 0;
             }
-        } // <--- TRÈS IMPORTANT : On ferme la boucle GAUCHE ici !
+        } 
 
-        // --- RESET AVANT LA DROITE ---
+        //  Reset des variables pour commencer  la boucle droite a 0
         IndexMurReference = TailleHistogramme - 1; 
         VolumeTemp = 0;
         ProfondeurMaxTemp = 0;
 
-        // 4. BOUCLE DROITE (Fin -> Sommet)
+        // boucle droite de la fin jusquau sommet Max
         for (int i = TailleHistogramme - 1; i >= IndexSommetMax; i--) { 
             
             if (Liste[i] < Liste[IndexMurReference]) {
-                VolumeTemp += (Liste[IndexMurReference] - Liste[i]);
+                VolumeTemp = VolumeTemp + (Liste[IndexMurReference] - Liste[i]);
                 int ProfondeurActuelle = Liste[IndexMurReference] - Liste[i];
                 if (ProfondeurActuelle > ProfondeurMaxTemp) {
                     ProfondeurMaxTemp = ProfondeurActuelle;
@@ -88,8 +91,8 @@ int main() {
                 if (VolumeTemp > 0) {
                     if (VolumeTemp > VolumeMax) {
                         VolumeMax = VolumeTemp;
-                        DebutVolume = i;                 // i est à gauche
-                        FinVolume = IndexMurReference;   // Ref est à droite
+                        DebutVolume = i;                 
+                        FinVolume = IndexMurReference;   
                     }
                     int LargeurActuelle = IndexMurReference - i - 1;
                     if (LargeurActuelle > LargeurMax) {
@@ -107,18 +110,18 @@ int main() {
                 VolumeTemp = 0;
                 ProfondeurMaxTemp = 0;
             }
-        } // Fin boucle Droite
+        } 
 
-        // 5. AFFICHAGE (LE JUDGE EN A BESOIN)
+        // Affichage des résultats
         if (VolumeMax == 0) {
             printf("0 0 0\n0 0 0\n0 0 0\n");
         } else {
-            // On ajoute +1 aux indices car l'énoncé compte à partir de 1
+            // +1 pour passer en base 1
             printf("%d %d %d\n", DebutVolume + 1, FinVolume + 1, VolumeMax);
             printf("%d %d %d\n", DebutLargeur + 1, FinLargeur + 1, LargeurMax);
             printf("%d %d %d\n", DebutProfondeur + 1, FinProfondeur + 1, ProfondeurMax);
         }
 
-    } // Fin du While
+    } 
     return 0;
 }
